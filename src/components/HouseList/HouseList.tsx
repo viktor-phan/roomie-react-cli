aa1e5df78760b27f727e0c7e725fd49c13913a50
viktor-phan <viktor.thang.phan@gmail.com>
2022-02-11 17:37:13 -0500

    Fixed css for navbar and Houselist

--- src/components/HouseList/HouseList.tsx
+++ src/components/HouseList/HouseList.tsx
@@ -3,7 +3,10 @@ import HouseCard from "../HouseCard";
 import "./houseList.css";
 export const HouseList = () => {
   return (
-    <div className="w-9/12 h-screen flex flex-row justify-center p-2">
+    <div className="w-9/12 h-screen flex flex-row flex-wrap justify-start p-2">
+      <HouseCard />
+      <HouseCard />
+      <HouseCard />
       <HouseCard />
       <HouseCard />
       <HouseCard />
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,15 +6,16 @@ export const NavBar = () => {
     <nav className="flex justify-between items-center h-16 bg-black text-white relative shadow-sm p-3 sticky top-0">
       <div className="text-3xl pl-8">
         <h3>Roomie</h3>
+        <img src="src/assets/logo.png" alt="logo" />
       </div>
       <div className="flex flex-row align-baseline">
-          <input type="text" placeholder="Search" className="w-4/5 h-12 round-sm pl-5" />
-          <a type="submit" className="w-12 h-12 bg-indigo-500">
+          <input type="text" placeholder="Search" className="w-4/5 h-12 rounded-l-md pl-5" />
+          <a type="submit" className="w-12 h-12 bg-indigo-500 rounded-r-md">
             <Search />
           </a>
       </div>
-      <div className="bg-indigo-500 hover:bg-indigo-200 rounded-sm pl-4 pr-4">
-        <a href="">Log In</a>
+      <div className="h-12 w-20 bg-indigo-500 hover:bg-indigo-200 rounded-md pl-4 pr-4">
+        <a className="m-auto" href="">Log In</a>
       </div>
     </nav>
   );
