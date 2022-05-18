fdbca79aa3f3a9ae4e1748de03df87f29008b929
viktor-phan <viktor.thang.phan@gmail.com>
2022-05-17 21:02:16 -0400

    Style revise

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import { Search } from "@mui/icons-material";
 import "./navBar.css";
 export const NavBar = () => {
+  const logoImage = require("../../assets/logo.png");
   return (
     <nav className="flex justify-between items-center h-16 bg-black text-white relative shadow-sm p-3 sticky top-0">
       <div className="text-3xl pl-8">
-        <h3>Roomie</h3>
-        <img src="src/assets/logo.png" alt="logo" />
+        <img src={logoImage} alt="logo" id="navbar__logo" />
       </div>
       <div className="flex flex-row align-baseline">
-          <input type="text" placeholder="Search" className="w-4/5 h-12 rounded-l-md pl-5" />
-          <a type="submit" className="w-12 h-12 bg-indigo-500 rounded-r-md">
-            <Search />
-          </a>
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-4/5 h-12 rounded-l-md pl-5"
+        />
+        <a type="submit" className="w-12 h-12 bg-indigo-500 hover:bg-indigo-400 rounded-r-md flex">
+          <Search className="m-auto" />
+        </a>
       </div>
-      <div className="h-12 w-20 bg-indigo-500 hover:bg-indigo-200 rounded-md pl-4 pr-4">
-        <a className="m-auto" href="">Log In</a>
+      <div className="h-12 w-28 bg-indigo-500 hover:bg-indigo-400 rounded-md pl-4 pr-4 align-middle flex">
+        <a className="m-auto" href="">
+          Log In
+        </a>
       </div>
     </nav>
   );
